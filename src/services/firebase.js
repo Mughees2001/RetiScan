a431d0db754dae221641c83af25c85649e2442e6
Saleha Muzammil <84681153+saleha-muzammil@users.noreply.github.com>
2024-03-30 22:40:00 +0500

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // Firebase configuration
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,                // Firebase API Key
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,        // Firebase Auth Domain
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,          // Firebase Project ID
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,  // Firebase Storage Bucket
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,  // Firebase Messaging Sender ID
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,                  // Firebase App ID
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID   // Firebase Measurement ID
+  apiKey: process.env["REACT_APP_FIREBASE_API_KEY"],
+  authDomain: process.env["REACT_APP_FIREBASE_AUTH_DOMAIN"],
+  projectId: process.env["REACT_APP_FIREBASE_PROJECT_ID"],
+  storageBucket: process.env["REACT_APP_FIREBASE_STORAGE_BUCKET"],
+  messagingSenderId: process.env["REACT_APP_FIREBASE_MESSAGING_SENDER_ID"],
+  appId: process.env["REACT_APP_FIREBASE_APP_ID"],
+  measurementId: process.env["REACT_APP_FIREBASE_MEASUREMENT_ID"]
 };
 
 let app;
